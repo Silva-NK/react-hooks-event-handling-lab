123da6a95bdd4a921ec53d3e6fa4da8fc334324f
Silva-NK <kiagonsuzanne@gmail.com>
2025-04-22 18:51:30 +0300

    Adds events to Keypad and EyesOnMe.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    
+    function handlePassChange() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
-    )
+        <div>
+            <input
+            type="password"
+            onChange={handlePassChange}
+            />
+        </div>
+    );
 }
 
 export default Keypad;
